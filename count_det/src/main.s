344114d66cc5fb7bda1a3ea1a3f446d4e7a7031e
Daria Babina <babina.daria.gmail.com>
2020-09-29 00:52:44 +0300

    added solution for the second task

--- count_det/src/main.s
+++ count_det/src/main.s
@@ -1,2 +1,42 @@
-/// Your code goes here...
+.text
 
+.global count_det
+
+count_det:
+        ldr r1, [r0,#0]
+	ldr r2, [r0,#16]
+	ldr r3, [r0,#32]
+	mul r2, r3, r2
+	mul r1, r2, r1
+        ldr r2, [r0,#4]
+        ldr r3, [r0,#20]
+	mul r2, r3, r2
+	ldr r3, [r0,#24]
+	mul r2, r3, r2
+	add r1, r2, r1
+	ldr r2, [r0,#8]
+	ldr r3, [r0,#12]
+	mul r2, r3, r2
+	ldr r3, [r0,#28]
+	mul r2, r3, r2
+	add r1, r2, r1
+	ldr r2, [r0,#8]
+	ldr r3, [r0,#16]
+	mul r2, r3, r2
+	ldr r3, [r0,#24]
+	mul r2, r3, r2
+	sub r1, r1, r2
+	ldr r2, [r0,#4]
+	ldr r3, [r0,#12]
+	mul r2, r3, r2
+	ldr r3, [r0,#32]
+	mul r2, r3, r2
+	sub r1, r1, r2
+	ldr r2, [r0,#0]
+	ldr r3, [r0,#20]
+	mul r2, r3, r2
+	ldr r3, [r0,#28]
+	mul r2, r3, r2
+	sub r1, r1, r2
+        mov r0, r1
+	bx lr
